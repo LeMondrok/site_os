8b85c59defa069b9726b58940c2b9cc037e12bfe
LeMondrok <dun2000@mail.ru>
2018-10-19 22:11:57 +0300

    debug

--- start.py
+++ start.py
@@ -5,8 +5,8 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-Messages = namedtuple('Message', 'text tag')
-messagess = []
+Message = namedtuple('Message', 'text tag')
+messages = []
 
 
 @app.route('/', methods=['GET'])
@@ -16,14 +16,14 @@ def hello_world():
 
 @app.route('/lul', methods=['GET'])
 def main():
-    return render_template('main.html', messages=messagess)
+    return render_template('main.html', messages=messages)
 
 
 @app.route('/add_message', methods=['POST'])
-def add_messages():
+def add_message():
     text = request.form['text']
     tag = request.form['tag']
 
-    messagess.append(Messages(text, tag))
+    messages.append(Message(text, tag))
 
     return redirect(url_for('main'))
